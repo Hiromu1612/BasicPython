6d44fa89e060b5fb3ef2d213a060b1f488f5956a
Hiromu1612 <150511546+Hiromu1612@users.noreply.github.com>
2024-03-29 15:24:39 +0900

    制御構文の解答

--- BasicPython-main/prime_number.py
+++ BasicPython-main/prime_number.py
@@ -2,3 +2,19 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+a = int(a)
+b = int(b)
+
+if a % 2 == 0:
+    print("素数ではありません")
+elif a == 1:
+    print("素数ではありません")
+else:
+    print("素数です")
+
+if b % 2 == 0:
+    print("素数ではありません")
+elif b == 1:
+    print("素数ではありません")
+else:
+    print("素数です")
