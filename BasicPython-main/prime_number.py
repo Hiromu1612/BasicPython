95a694503b34da634a1a6fa5a277c46284931657
Hiromu1612 <150511546+Hiromu1612@users.noreply.github.com>
2024-03-30 00:51:44 +0900

    2を素数判定するようにしました

--- BasicPython-main/prime_number.py
+++ BasicPython-main/prime_number.py
@@ -5,14 +5,18 @@ b = input("bの値を入力: ")
 a = int(a)
 b = int(b)
 
-if a % 2 == 0:
+if a == 2:
+    print("素数です")
+elif a % 2 == 0:
     print("素数ではありません")
 elif a == 1:
     print("素数ではありません")
 else:
     print("素数です")
 
-if b % 2 == 0:
+if a == 2:
+    print("素数です")
+elif b % 2 == 0:
     print("素数ではありません")
 elif b == 1:
     print("素数ではありません")
