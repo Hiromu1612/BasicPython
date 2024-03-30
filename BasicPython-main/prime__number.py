4c9af210a1fd5f5ff459c633372e2ed34ff4f8b4
Hiromu1612 <150511546+Hiromu1612@users.noreply.github.com>
2024-03-30 20:46:57 +0900

    修正

---
+++ BasicPython-main/prime__number.py
@@ -0,0 +1,24 @@
+# a = input("aの値を入力: ")
+# b = input("bの値を入力: ")
+# TODO
+n = input("nの値を入力: ")
+try:
+    n = int(n)
+except:
+    print("nは整数で入力してください")
+
+def prime_number(n):
+    if n < 2:
+        return "素数ではありません"
+    else:
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+            elif i == n-1:
+                return True
+
+result = prime_number(n)
+if result is None:
+    result = True
+
+print(result)
