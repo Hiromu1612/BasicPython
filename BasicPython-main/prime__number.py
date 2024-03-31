198b371d5b9c5ce23e67b7949ea5ac7fbe0609c9
Hiromu1612 <150511546+Hiromu1612@users.noreply.github.com>
2024-03-31 21:45:13 +0900

    merge conflictの解消

---
+++ BasicPython-main/prime__number.py
@@ -0,0 +1,25 @@
+# a = input("aの値を入力: ")
+# b = input("bの値を入力: ")
+# TODO
+n = input("nの値を入力: ")
+try:
+    n = int(n)
+except:
+    print("nは整数で入力してください")
+    exit()
+
+def prime_number(n):
+    if n < 2:
+        return False
+    else:
+        for i in range(2, n):
+            if i == n-1:
+                return True
+            elif n % i == 0:
+                return False
+
+result = prime_number(n)
+if result is None: #これがないと2がNoneになる
+    result = True
+
+print(result)
--- BasicPython-main/prime_number.py
+++ BasicPython-main/prime_number.py
@@ -2,6 +2,7 @@
 # b = input("bの値を入力: ")
 
 # TODO
+<<<<<<< HEAD
 a = int(a)
 b = int(b)
 
@@ -18,3 +19,18 @@ elif b == 1:
     print("素数ではありません")
 else:
     print("素数です")
+=======
+import math
+
+n = int(input("nの値を入力: "))
+
+if n <= 1:
+    print("素数ではありません")
+else:
+    for i in range(2, math.isqrt(n) + 1): #2からその数の平方根までの全ての数で割り切れるかどうかを調べる
+        if n % i == 0:
+            print("素数ではありません")
+            break
+    else:
+        print("素数です")
+>>>>>>> 61eac50c7235b2cf8421d34a41983b7e83c3b2ef
